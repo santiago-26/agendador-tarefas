0be41cb9233e37cf23a9885f8bea74854af8a781
Mauricio Santiago <santiagodev6@hotmail.com>
2026-03-04 19:53:44 -0300

    commit inicial

---
+++ src/test/java/com/santiago_labs/agendador_tarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago_labs.agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
